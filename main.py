46204196f5d663e5b5eec8f73b8d66659c06dbe8
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-07-01 00:58:59 +0200

    simplest main functions
    imported relevant functions,
    spaced out stuff,
    initializes game

--- main.py
+++ main.py
@@ -1,39 +1,40 @@
 # import the pygame module, so you can use it
 import pygame
 
+from gameInit import gameInit
+from gameLoop import gameLoop
+
+# ---------------------------------------------------------------------
+
 def initPygame():
+
     # initialize the pygame module
     pygame.init()
+    
     # load and set the logo
     ##not set yet
     #logo = pygame.image.load("logo32x32.png")
     #pygame.display.set_icon(logo)
     pygame.display.set_caption("Bored Games")
+    
     # create a surface on screen that has the size of 240 x 180
     screen = pygame.display.set_mode((720,480))
     return screen
 
-
-running = True
-def loopPygame(screen):
-    while running:
-        # event handling, gets all event from the event queue
-        for event in pygame.event.get():
-            # only do something if the event is of type QUIT
-            if event.type == pygame.QUIT:
-                # change the value to False, to exit the main loop
-                running = False
+# ---------------------------------------------------------------------
 
 # define a main function
 def main():
      
     screen = initPygame()
 
-    # define a variable to control the main loop
-    running = True
-    # main loop
-    loopPygame(screen)
+    # Initialize game
+    game = gameInit();
 
+    # Start the game loop
+    gameLoop(screen, game)
+
+# ---------------------------------------------------------------------
 
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
