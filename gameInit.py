1f2b70d591b06bf0c4b60118a0d840081b1f8673
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-07-01 00:57:35 +0200

    added simple gameinitialization

---
+++ gameInit.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+
+@dataclass
+class Game:
+    cameraX: int
+    cameraY: int
+
+    posX: int
+    posY: int
+
+    map: list
+
+
+def gameInit():
+    
+    map = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ]
+
+    game = Game(0, 0, 0, 0, map)
+    return game
