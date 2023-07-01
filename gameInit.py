2974509bd445d7cc5b2670dcb187ed82d9b5ba51
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-07-01 15:01:03 +0200

    added player pngs

--- gameInit.py
+++ gameInit.py
@@ -37,8 +37,12 @@ def gameInit():
         0,
         map,
         {
-            "floorTile": pygame.image.load("gameSprites/floorTile.png")
+            "floorTile": pygame.image.load("gameSprites/floorTile.png"),
+            "player1": pygame.image.load("gameSprites/player1.png"),
+            "player2": pygame.image.load("gameSprites/player2.png"),
+            "player3": pygame.image.load("gameSprites/player3.png"),
+            "player4": pygame.image.load("gameSprites/player4.png")
         }
-        
+
     )
     return game
