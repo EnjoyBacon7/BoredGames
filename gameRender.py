b732b1cda16c62bcf9ecfa6d15657a49bd089742
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-07-01 15:02:22 +0200

    modified render game with sprite width in mind

--- gameRender.py
+++ gameRender.py
@@ -4,11 +4,16 @@ def renderGame(window, game):
     # Handle events so the window doesn't freeze
     pygame.event.pump()
 
-    window.fill((0,0,0))
+    # Render Functions
+    window.fill((230,255, 255))
+    renderBoard(window, game)
+
+    pygame.display.flip()
+
+# ---------------------------------------------------------------------
+
+def renderBoard(window, game):
     for y in range(0, len(game.map)):
         for x in range(0, len(game.map[y])):
             if game.map[y][x] == 1:
-                pygame.Surface.blit(window, game.sprites["floorTile"], (x*100, y*100))
-
-
-    pygame.display.flip()
+                pygame.Surface.blit(window, game.sprites["floorTile"], (x*game.sprites["floorTile"].get_width(), y*game.sprites["floorTile"].get_height()))
