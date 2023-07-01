d0ab77cb63c715cdcfb59feffca877de3ee934b3
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-07-01 12:29:27 +0200

    displays floorTiles now

--- gameRender.py
+++ gameRender.py
@@ -1,12 +1,14 @@
 import pygame
 
-def renderGame(screen, game):
+def renderGame(window, game):
     # Handle events so the window doesn't freeze
     pygame.event.pump()
-    screen.fill((0,0,0))
+
+    window.fill((0,0,0))
     for y in range(0, len(game.map)):
         for x in range(0, len(game.map[y])):
             if game.map[y][x] == 1:
-                pygame.draw.rect(screen, (255,255,255), (x*20, y*20, 10, 10))
-                print("Drawing tile at " + str(x*80) + ", " + str(y*80))
+                pygame.Surface.blit(window, game.sprites["floorTile"], (x*100, y*100))
+
+
     pygame.display.flip()
