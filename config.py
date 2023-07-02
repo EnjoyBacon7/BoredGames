94014cafb9879cbc74f7a910864a68ec5b14ddee
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-07-02 19:36:12 +0200

    added more button configs

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
 resolution_height = 1080
 resolution_width = 1920
 
+# Menu object scaling
+
+menu_scale = 8
+
 # Main Menu object Placement in percent
 
-play_x = 50
+play_x = 80
 play_y = 50
+
+option_x = 80
+option_y = 60
+
+quit_x = 80
+quit_y = 70
