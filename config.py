15b97da60b4714b48febf8e7119384c87ad38135
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-07-05 15:26:33 +0200

    changed config positions to be absolute
    when compared to resolution

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ menu_scale = 8
 
 # Main Menu object Placement in percent
 
-play_x = 80
-play_y = 50
+play_x = (80/100)*resolution_width
+play_y = (50/100)*resolution_height
 
-option_x = 80
-option_y = 60
+option_x = (80/100)*resolution_width
+option_y = (60/100)*resolution_height
 
-quit_x = 80
-quit_y = 70
+quit_x = (80/100)*resolution_width
+quit_y = (70/100)*resolution_height
