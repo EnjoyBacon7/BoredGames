89063f3f1add62232379a6fbc29da7844770ddad
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-07-07 11:09:19 +0200

    Added convert_alpha() to images for optimization

--- Python/gameInit.py
+++ Python/gameInit.py
@@ -12,6 +12,7 @@ class Game:
     posY: int
 
     map: list
+    
     collisions: list
     sprites: dict
 
@@ -19,7 +20,8 @@ class Game:
 
 def gameInit():
 
-    map = pygame.image.load("Content/world/level.png")
+    map = pygame.image.load("Content/world/level.png").convert_alpha()
+
 
     collisions = []
     with open("Content/world/collisions.csv") as csv_file:
@@ -28,11 +30,11 @@ def gameInit():
             collisions.append(row)
 
     sprites = {
-        "floorTile": pygame.image.load("Content/Images/floorTile.png"),
-        "player1": pygame.image.load("Content/Images/player1.png"),
-        "player2": pygame.image.load("Content/Images/player2.png"),
-        "player3": pygame.image.load("Content/Images/player3.png"),
-        "player4": pygame.image.load("Content/Images/player4.png")
+        "floorTile": pygame.image.load("Content/Images/floorTile.png").convert_alpha(),
+        "player1": pygame.image.load("Content/Images/player1.png").convert_alpha(),
+        "player2": pygame.image.load("Content/Images/player2.png").convert_alpha(),
+        "player3": pygame.image.load("Content/Images/player3.png").convert_alpha(),
+        "player4": pygame.image.load("Content/Images/player4.png").convert_alpha(),
     }
     
     game = Game(0, 0, 1, 1, map, collisions, sprites, 0)
