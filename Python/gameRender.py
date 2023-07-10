af8a1c1f38958497cd7d41075e954e41877c5f8c
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-07-10 18:46:25 +0200

    added scaled map cache to avoid scaling on every frame
    also added (in comments) WIP for image culling with game world

--- Python/gameRender.py
+++ Python/gameRender.py
@@ -41,10 +41,22 @@ def updateCamera(game):
 # change origin to center of screen: screen_position = (x_screen_position + (cs.resolution_width/2), y_screen_position + (cs.resolution_height/2))
 
 def renderBoard(window, game):
-    scaled_map = pygame.transform.scale(game.map, (cs.zoom * 50 * 16, cs.zoom * 50 * 16))
+
+    # WIP
+    # map_view = pygame.Rect(game.cameraX, game.cameraY, 1/cs.zoom * 16, 1/cs.zoom * 16)
+    # sub_img = game.map.subsurface(map_view)
+    # scaled_view = pygame.transform.scale(sub_img, (cs.resolution_width, cs.resolution_height))
+
+    # window.blit(scaled_view, coordsToPixels(0, 0))
+
+    if(game.map_cache_factor != cs.zoom):
+        game.map_cache = pygame.transform.scale(game.map, (cs.zoom * 50 * 16, cs.zoom * 50 * 16))
+        game.map_cache_factor = cs.zoom
+    scaled_map = game.map_cache
     screen_position = coordsToPixels(0 - game.cameraX, 0 - game.cameraY)
     screen_position = (screen_position[0] + (cs.resolution_width/2), screen_position[1] + (cs.resolution_height/2))
     window.blit(scaled_map, (screen_position))
+
 
 def renderPlayer(window, game):
     scaled_player = pygame.transform.scale(game.sprites["player"], (cs.zoom * 8, cs.zoom * 16))
